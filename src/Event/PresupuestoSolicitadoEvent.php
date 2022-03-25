ea93aa4a763763bce8677a78100f85c56bb285a3
carherco <carherco@gmail.com>
2022-03-25 17:21:12 +0100

    Comentarios solución 9.3

--- src/Event/PresupuestoSolicitadoEvent.php
+++ src/Event/PresupuestoSolicitadoEvent.php
@@ -5,4 +5,14 @@ namespace App\Event;
 class PresupuestoSolicitadoEvent extends PresupuestoEvent
 {
     public const NAME = 'presupuesto.solicitado';
+
+    private $solicitudPresupuesto;
+
+    public function __construct(SolicitudPresupuesto $solicitudPresupuesto) {
+        $this->solicitudPresupuesto = $solicitudPresupuesto;
+    }
+
+    public function getPresupuesto(): SolicitudPresupuesto {
+        return $this->solicitudPresupuesto;
+    }
 }

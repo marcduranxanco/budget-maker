237fac00898248f2b1bd2ab4c8836ce3cf5e6995
marcduranxanco <marcduranxanco@hotmail.com>
2022-03-28 00:33:44 +0200

    [15.2] - Actualiza las fixtures añadiendo todos los usuarios de prueba

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,54 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct (UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        foreach($this->crearUsuarios() as $usuario)
+        {
+            $manager->persist($usuario);
+        };
         $manager->flush();
+    }
+
+    /**
+     * @return array<User>
+     */
+    private function crearUsuarios(): array
+    {
+        $admin = new User();
+        $admin->setEmail('admin@curso.local');
+        $hash = $this->passwordHasher->hashPassword($admin, 'admin');
+        $admin->setPassword($hash);
+
+        $comercial = new User();
+        $comercial->setEmail('comercial@curso.local');
+        $hash = $this->passwordHasher->hashPassword($comercial, 'comercial');
+        $comercial->setPassword($hash);
+
+        $jefeproyecto = new User();
+        $jefeproyecto->setEmail('jefeproyecto@curso.local');
+        $hash = $this->passwordHasher->hashPassword($jefeproyecto, 'jefeproyecto');
+        $jefeproyecto->setPassword($hash);
+
+        $empleado = new User();
+        $empleado->setEmail('empleado@curso.local');
+        $hash = $this->passwordHasher->hashPassword($empleado, 'empleado');
+        $empleado->setPassword($hash);
+
+        return [$admin, $comercial, $jefeproyecto, $empleado];
     }
 }

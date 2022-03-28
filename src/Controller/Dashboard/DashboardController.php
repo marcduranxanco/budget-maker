7af4a11aeef0e3b5145830c9daca7e257ec61999
marcduranxanco <marcduranxanco@hotmail.com>
2022-03-28 23:59:07 +0200

    [15.2] - Limita el acceso al dashboard en función del rol del usuario que accede

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -15,21 +15,25 @@ class DashboardController extends AbstractController
 
     private function admin(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->json('Panel Admin');
     }
 
     private function comercial(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_COMERCIAL');
         return $this->json('Panel Comercial');
     }
 
     private function jefeproyecto(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_JEFEPROYECTO');
         return $this->json('Panel Jefe de Proyecto');
     }
 
     private function empleado(): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_EMPLEADO');
         return $this->json('Panel Empleado');
     }
 }

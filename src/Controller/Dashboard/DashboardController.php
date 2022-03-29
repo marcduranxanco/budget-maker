3ac74eb51ac0d1785c61fa0ee4b6fa4951c666f5
marcduranxanco <marcduranxanco@hotmail.com>
2022-03-29 20:35:33 +0200

    [15.2] - Renderiza las respuestas del dashboard desde el twig

--- src/Controller/Dashboard/DashboardController.php
+++ src/Controller/Dashboard/DashboardController.php
@@ -16,24 +16,36 @@ class DashboardController extends AbstractController
     private function admin(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->json('Panel Admin');
+        return $this->render('dashboard/base_dashboard.html.twig', [
+            'panel_type' => 'administrador',
+            'controller_name' => 'IndexController',
+        ]);
     }
 
     private function comercial(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_COMERCIAL');
-        return $this->json('Panel Comercial');
+        return $this->render('dashboard/base_dashboard.html.twig', [
+            'panel_type' => 'comercial',
+            'controller_name' => 'IndexController',
+        ]);
     }
 
     private function jefeproyecto(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_JEFEPROYECTO');
-        return $this->json('Panel Jefe de Proyecto');
+        return $this->render('dashboard/base_dashboard.html.twig', [
+            'panel_type' => 'jefe de proyecto',
+            'controller_name' => 'IndexController',
+        ]);
     }
 
     private function empleado(): Response
     {
         $this->denyAccessUnlessGranted('ROLE_EMPLEADO');
-        return $this->json('Panel Empleado');
+        return $this->render('dashboard/base_dashboard.html.twig', [
+            'panel_type' => 'empleado',
+            'controller_name' => 'IndexController',
+        ]);
     }
 }

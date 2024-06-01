195c415ace428a29467d303c63365e923a8e0ebd
mduranx <mduranx@hiberus.com>
2024-06-01 21:01:55 +0200

    Presupuesto - Crea el proyecto al aprobar un presupuesto

--- src/EventSubscriber/PresupuestoEventSubscriber.php
+++ src/EventSubscriber/PresupuestoEventSubscriber.php
@@ -2,18 +2,22 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\Proyecto;
 use App\Event\Presupuesto\PresupuestoAprobadoEvent;
 use App\Event\Presupuesto\PresupuestoSolicitadoEvent;
 use App\Services\EmailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class PresupuestoEventSubscriber implements EventSubscriberInterface
 {
     private EmailService $emailService;
+    private EntityManagerInterface $entityManager;
 
-    public function __construct(EmailService $emailService)
+    public function __construct(EmailService $emailService, EntityManagerInterface $entityManager)
     {
         $this->emailService = $emailService;
+        $this->entityManager = $entityManager;
     }
 
     public static function getSubscribedEvents() : array
@@ -38,5 +42,13 @@ class PresupuestoEventSubscriber implements EventSubscriberInterface
     {
         $presupuesto = $event->getPresupuesto();
         $this->emailService->enviarCorreosPresupuestoAprobado($presupuesto);
+
+        $proyecto = new Proyecto();
+        $proyecto->setPresupuesto($presupuesto);
+        $fechaEntrega = new \DateTime();
+        $fechaEntrega->modify('+1 year');
+        $proyecto->setFechaEntrega($fechaEntrega);
+        $this->entityManager->persist($proyecto);
+        $this->entityManager->flush();
     }
 }
